a5ffe4a048d02f52482f00e9b9f72b31035fa026
rdelafuente <rdelafuente@atsistemas.com>
2015-08-29 17:50:47 +0200

    Password confirm directive

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 This is a collection of AngularJS directives I usually need in my projects and can't find anywhere. It includes:
 
 - **prepend-http-directive**, which will auto-prepend 'http' to any input field you want when the user starts typing. This seems lame, but the input[type=url] won't validate otherwise.
+- **password-confirm-directive**, makes an input field a password confirmation field. Taken from http://odetocode.com/blogs/scott/archive/2014/10/13/confirm-password-validation-in-angularjs.aspx
---
+++ angular-password-confirm-directive.js
@@ -0,0 +1,25 @@
+(function(angular){
+    angular
+        .module("passwordConfirmDirective", ['ngModel'])
+        .directive('compareTo', compareTo);
+
+    function compareTo() {
+        return {
+            require: "ngModel",
+            scope: {
+                otherModelValue: "=compareTo"
+            },
+            link: function(scope, element, attributes, ngModel) {
+
+                ngModel.$validators.compareTo = function(modelValue) {
+                    return modelValue == scope.otherModelValue;
+                };
+
+                scope.$watch("otherModelValue", function() {
+                    ngModel.$validate();
+                });
+            }
+        };
+    }
+})(angular);
+
